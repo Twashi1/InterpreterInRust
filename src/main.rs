dae06a36faa96cf26f35425ebadc86d62f8e6bd5
twashi1 <thomas.zarri@durham.ac.uk>
2024-02-28 17:50:51 +0000

    Multiple terms executing

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,128 @@
 use std::{error, string};
 
-enum Operator {
-    ADD,
-    SUB,
-    DIV,
-    MUL
+enum TokenType {
+    NUMBER,
+    OPERATOR
 }
 
-enum State {
-    INTEGER_BEGIN,
-    INTEGER,
-    OPERATION
+union TokenData {
+    value: f32,
+    operator: u8
 }
 
-fn calc(operand1 : i32, operand2 : i32, operation : Operator) -> i32 {
+struct Token {
+    data_type: TokenType,
+    data: TokenData
+}
+
+fn calc(operand1 : f32, operand2 : f32, operation : u8) -> f32 {
     match operation {
-        Operator::ADD => return operand1 + operand2,
-        Operator::SUB => return operand1 - operand2,
-        Operator::DIV => return operand1 / operand2,
-        Operator::MUL => return operand1 * operand2
+        b'+' => return operand1 + operand2,
+        b'-' => return operand1 - operand2,
+        b'/' => return operand1 / operand2,
+        b'*' => return operand1 * operand2,
+        _ => return 0.0
     }
 }
 
-fn exec(value : String) -> i32 {
-    if value.len() == 0 { return 0; }
+fn readNumber(string : &String, pos : &mut usize) -> Token {
+    let mut currentValue : f32 = 0.0;
+    let mut digitsAfterDecimal : i32 = 0;
+    
+    while *pos < string.len() {
+        let currentChar : u8 = string.as_bytes()[*pos];
 
-    let mut state : State = State::INTEGER_BEGIN;
-    let mut currentValue : i32 = 0;
-    let mut isNegative : bool = false;
-    let mut operation : Operator = Operator::ADD;
-    let mut store : Vec<i32> = Vec::new();
-
-    let integerConvertClosure = | currentChar : u8, currentValue : &mut i32 | -> bool
-    {
-        // Check character is numeric
-        if currentChar >= '0' as u8 && currentChar <= '9' as u8 {
-            // Add to currentValue
-            *currentValue = *currentValue * 10;
-            *currentValue = *currentValue + (currentChar - '0' as u8) as i32;
-
-            return true;
-        }
-
-        return false;
-    };
-
-    let errorClosure = |i : usize, msg : &str| -> () { println!("Parsing error idx {i}: {msg}"); };
-
-    let mut i : usize = 0;
-
-    while i < value.len() {
-        let currentChar : u8 = value.as_bytes()[i];
-
-        // No fallthrough, sad
-        match state {
-            State::INTEGER_BEGIN => {
-                isNegative = currentChar == '-' as u8;
+        match currentChar {
+            b'0'..=b'9' => {
+                let digitValue = currentChar - b'0';
                 
-                integerConvertClosure(currentChar, &mut currentValue);
-
-                // Transition to integer
-                state = State::INTEGER;
-            }
-            State::INTEGER => {
-                let wasDigit = integerConvertClosure(currentChar, &mut currentValue);
-
-                // Assume transition to operation
-                if !wasDigit {
-                    store.push(currentValue * (!isNegative as i32 * 2 - 1));
-                    currentValue = 0;
-                    i -= 1;
-
-                    state = State::OPERATION;
-                }
-            },
-            State::OPERATION => {
-                match currentChar as char {
-                    '+' => { state = State::INTEGER_BEGIN; operation = Operator::ADD; },
-                    '-' => { state = State::INTEGER_BEGIN; operation = Operator::SUB; },
-                    '*' => { state = State::INTEGER_BEGIN; operation = Operator::MUL; },
-                    '/' => { state = State::INTEGER_BEGIN; operation = Operator::DIV; },
-                    ' ' => {},
-                    _ => errorClosure(i, "Expected operation")
+                if digitsAfterDecimal == 0 {
+                    currentValue *= 10.0;
+                    currentValue += digitValue as f32;
+                } else {
+                    currentValue += digitValue as f32 * (10 as f32).powf(-digitsAfterDecimal as f32) as f32;
+                    digitsAfterDecimal += 1;
                 }
             }
+            b'.' => {
+                if digitsAfterDecimal > 0 {
+                    panic!("Multiple decimal points inside number");
+                }
+
+                digitsAfterDecimal = 1;
+            }
+            _ => return Token {data_type: TokenType::NUMBER, data: TokenData { value: currentValue }}
         }
 
-        i += 1;
+        *pos += 1;
     }
 
-    store.push(currentValue * (!isNegative as i32 * 2 - 1));
-
-    if store.len() >= 2 {
-        return calc(store[0], store[1], operation);
+    if currentValue != 0.0 {
+        return Token { data_type: TokenType::NUMBER, data: TokenData{value : currentValue} }
     }
     
-    return 0;
+    panic!("Didn't read integer, or reached EOF");
+}
+
+fn lex(string : &String) -> Vec<Token> {
+    let mut tokens : Vec<Token> = Vec::new();
+
+    let mut currentPos : usize = 0;
+
+    while currentPos < string.len() {
+        let currentChar : u8 = string.as_bytes()[currentPos];
+
+        match currentChar {
+            b'0'..=b'9' | b'.' => {
+                tokens.push(readNumber(string, &mut currentPos));
+                currentPos -= 1;
+            }
+            b'+' | b'-' | b'*' | b'/' => {
+                tokens.push(Token{ data_type: TokenType::OPERATOR, data: TokenData {operator: currentChar as u8} });
+            }
+            _ => {}
+        }
+
+        currentPos += 1;
+    }
+
+    return tokens;
+}
+
+fn parse_and_exec(token_stream: &Vec<Token>) -> f32 {
+    let mut currentIndex : usize = 0;
+
+    let mut currentResult : f32 = 0.0;
+
+    let mut lhs: &Token = &token_stream[currentIndex];
+    let mut op: &Token = &token_stream[currentIndex + 1];
+    let mut rhs: &Token = &token_stream[currentIndex + 2];
+
+    // Perform operation
+    unsafe {
+        currentResult = calc(lhs.data.value, rhs.data.value, op.data.operator);
+    }
+
+    currentIndex += 3;
+
+    while currentIndex < token_stream.len() {
+        let operator: &Token = &token_stream[currentIndex];
+        let operand : &Token = &token_stream[currentIndex + 1];
+
+        unsafe {
+            currentResult = calc(currentResult, operand.data.value, operator.data.operator);
+        }
+
+        currentIndex += 2
+    }
+    
+    return currentResult;
 }
 
 fn main() {
-    let result = exec("-35*-56".to_string());
+    let input : String = String::from("1+2*3");
+    let tokens: Vec<Token> = lex(&input);
+    let result : f32 = parse_and_exec(&tokens);
 
-    println!("{result}");
+    println!("Res: {result}");
 }
